233d53d33faefd5e0063aedd6dd5d7bdce093315
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2021-07-15 00:05:24 -0300

    Desafio Concluido

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,21 +15,26 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    setTasks([...tasks, { 
-      id: new Date().getTime(), 
-      title: newTaskTitle, 
-      isComplete: false 
+    setTasks([...tasks, {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      isComplete: false
     }])
+    setNewTaskTitle('')
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
-    const taskAtual = tasks.filter(task => task.id === id)
-    console.log(taskAtual)
+    const updateTask = tasks.map(task => 
+      task.id === id ? { ...task, isComplete: !task.isComplete } : task
+    )
+
+    setTasks(updateTask)
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const removeTask = tasks.filter(task => task.id !== id)
+
+    setTasks(removeTask)
   }
 
   return (
